f5cfe43e9f4c3e95160fb9b0b84e48199a48a670
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-06-11 16:37:36 +0300

    add installation of zsh and oh-my-zsh to Dockerfile

--- c_dev/build_image.sh
+++ c_dev/build_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 IMAGE_NAME="valgrind-image"
-DOCKERFILE_PATH="docker_setups/c_dev"
+DOCKERFILE_PATH="docker_setups/c_dev/Dockerfile"
 
 docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
